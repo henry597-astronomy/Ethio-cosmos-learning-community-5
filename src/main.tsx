3528860c83778db475ec2fcb295f72d33d64b3bb
henry597-astronomy <2.80658729e+08+henry597-astronomy@users.noreply.github.com>
2026-08-14 14:37:37 +0000

    fix: remove unused CapApp import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App as CapApp } from '@capacitor/app';
 import './index.css'
 import { disableLongPressContextMenu } from '@/lib/disable-long-press';
 
